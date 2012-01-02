6756450036253fd71f23de2e976a18f1638eb0f3
Lujaw Shrestha <lujaw@hotmail.com>
2012-01-02 15:08:47 +0545

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Criminaldb::Application.config.secret_token = '142a767cc0810df6faf6be8fbc3f0e4c971d7091cbdc92b41d074dfb54a7971c0c3541624ef4adc68e89858f2a5e9bce88bc118cd73ec4b71af6f0fddc13c700'
